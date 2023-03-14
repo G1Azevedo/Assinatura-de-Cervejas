c81521d5e5934dad35b3f38522c951759bdca515
Gabriel Azevedo <gabriel.araujo2706@gmail.com>
2023-03-14 19:48:39 -0300

    Informações de desenvolvidor

--- assinatura.c
+++ assinatura.c
@@ -1,3 +1,12 @@
+/////////////////////////////////////////////////////////////////////////////
+//             Universidade Federal do Rio Grande do Norte                 //
+//                 Centro de Ensino Superior do Seridó                     //
+//               Departamento de Computação e Tecnologia                   //
+//                  Disciplina DCT1106 -- Programação                      //
+//                  Projeto Sistema de Gestão Escolar                      //
+//             Desenvolvido por @G1Azevedo -- since Mar, 2023              //
+/////////////////////////////////////////////////////////////////////////////
+
 #include <stdio.h>
 
 int main(void) {
