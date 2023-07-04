67e4772da054d9adf9d41858e755940531446e78
HumanoGeovs <shinytopazio@gmail.com>
2023-07-04 10:33:18 -0300

    NOVO MAIN/ADD SOBRE

---
+++ sobre.c
@@ -0,0 +1,59 @@
+//Sobre & equipe
+
+void tela_sobre(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///                  Projeto Sistema de Assinatura de Cervejas              ///\n");
+    printf("///             Desenvolvido por @G1Azevedo -- since Mar, 2023              ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///                          🇧​ 🇷 ​🇪​ 🇯​ 🇦 ​🇧​ 🇴​ 🇽​                                ///\n");
+    printf("///                                                                         ///\n");
+    printf("///  Programa em desenvolvimento com foco em apresentar um projeto          ///\n");
+    printf("///  funcional para Assinatura de Cervejas. Baseado no programa de Gestão   ///\n");
+    printf("///  Escolar, através deste projeto tentarei desenvolver o que se pede ao   ///\n");
+    printf("///  decorrer da matéria e aprender o nível necessário da linguagem para    ///\n");
+    printf("///  que assim possa dar as funcionalidades fundamentais a este programa.   ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void tela_equipe(void) {
+
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///                  Projeto Sistema de Gestão Escolar                      ///\n");
+    printf("///             Developed by @G1Azevedo -- since Mar, 2023                  ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = Sistema de Gestão Escolar = = = = =                ///\n");
+    printf("///                                   modulo_cliente                                      ///\n");
+    printf("///            Este projeto exemplo foi desenvolvido por:                   ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Gabriel Azevedo de Araujo                                    ///\n");
+    printf("///            E-mail: gabriel.araujo2706@gmail.com                         ///\n");
+    printf("///            Redes sociais: @gabriel_azvd0                                ///\n");
+    printf("///            Git: https://github.com/G1Azevedo/Assinatura-de-Cervejas     ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+} 
